a1e102c2ff2a22ef552c6382a01cb702bd802a34
bodevgit <49619@hoornbeeck.nl>
2026-02-16 21:13:41 +0100

    Update app icons from user image

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png'],
       manifest: {
         name: 'Angy & Bozy App',
         short_name: 'AngyBozy',
